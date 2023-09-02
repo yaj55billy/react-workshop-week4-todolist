09c6880c92bd568c16ad9c45f7e9488f1dcba813
yaj55billy <yaj55ushydna@gmail.com>
2023-09-02 15:09:32 +0800

    adjust vite config base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	// 產品路徑、開發路徑
-	base: process.env.NODE_ENV === "production" ? "/react-workshop-week4/" : "/",
+	base:
+		process.env.NODE_ENV === "production"
+			? "/react-workshop-week4-todolist/"
+			: "/",
 	plugins: [react()],
 });
